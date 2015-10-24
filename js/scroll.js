a992c3684940510bcb7af7ebc03a7cc5644ac319
Alex Hoffnung <alex.hoffnung@gmail.com>
2015-10-24 16:49:05 -0400

    Add scroll.js to fix nav link offset issue

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('div.menubox > a').click(function(e) {
+		//Prevent a page reload when a link is pressed
+		e.preventDefault();
+		//Call the scroll function
+		goToByScroll($(this).attr("href"));
+	});
+});
+
+function goToByScroll(id){
+	//Scroll
+	console.log($('#' + id).offset().top)
+	$("html,body").animate({ scrollTop: $('#' + id).offset().top - 160}, 'slow');
+};
